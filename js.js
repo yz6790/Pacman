7896428e06a4d11ffbcadf941e7dfe1d92ffaf1e
Youyu Zhu <yz6790@nyu.edu>
2022-08-29 23:08:57 -0400

    8/29

--- js.js
+++ js.js
@@ -1,3 +1,27 @@
-var canvas=document.getElementById('canvasspace');
-var ctx=canvas.getContext("2d");
-ctx.fillText("hello",10,150);
+
+var canvas=document.createElement("canvas");
+var context=canvas.getContext("2d");
+canvas.height=100%;
+canvas.width=100%;
+mainImage=new Image();
+mainImage.ready = false;
+mainImage.onload = checkReady;
+mainImage.src="pac.png";
+
+
+function checkReady(){
+    this.ready=true;
+    playgame();
+}
+
+function playgame(){
+    render();
+}
+
+function render(){
+    context.fillStyle = "black";
+    context.fillRect(0,0,canvas.width,canvas.height);
+    context.drawImage(mainImage,320,0,32,32,50,50,32,32);
+}
+
+document.body.appendChild(canvas);
